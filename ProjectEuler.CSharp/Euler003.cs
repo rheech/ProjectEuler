e7942c1357c26dfa9774c601ab7e098c85eb7170
Cheonghyun Lee <lee@cheonghyun.com>
2017-03-16 10:17:35 +0900

    Solve Euler #3

---
+++ ProjectEuler.CSharp/Euler003.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.CSharp
+{
+    class Euler003
+    {
+        // Project Euler Problem #3: Largest prime factor
+        // The prime factors of 13195 are 5, 7, 13 and 29.
+        // What is the largest prime factor of the number 600851475143?
+
+        static void Main(string[] args)
+        {
+            decimal answer = GetLargestPrimeFactorOf(600851475143);
+            Console.WriteLine("The largest prime factor of the number 600851475143 is: {0}", answer);
+            Console.ReadLine();
+        }
+
+        static decimal GetLargestPrimeFactorOf(decimal bound)
+        {
+            decimal i;
+
+            for (i = 2; i < bound; i++)
+            {
+                if (bound % i == 0)
+                {
+                    bound /= i;
+                }
+            }
+
+            return i;
+        }
+    }
+}
---
+++ ProjectEuler.FSharp/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+let foo n =
+    n * n + n + 41
+
+[<EntryPoint>]
+let main argv = 
+    for i in 1 .. 50 do
+        let x = foo(i)
+        printfn "x: %d" x
+
+    0 // return an integer exit code
